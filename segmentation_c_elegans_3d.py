530ea1ddd58cf9176e3691e261996fe81ba9be45
David <davidcking.inbox@gmail.com>
2023-08-09 15:00:13 -0600

    Added title to segmentation figure

--- segmentation_c_elegans_3d.py
+++ segmentation_c_elegans_3d.py
@@ -265,7 +265,7 @@ plt.close()
 #plt.figure(figsize=(5,4))
 spots_detected_dataframe = tp.locate(GFP,diameter=particle_size, minmass=400) # "spots_detected_dataframe" is a pandas data freame that contains the infomation about the detected spots
 img = tp.annotate(spots_detected_dataframe, image=GFP, plot_style={'markersize': 1.5})  # tp.anotate is a trackpy function that displays the image with the detected spots
-            
+img.title.set_text(f"{datestr}_{genotype}_{RNAi}_{repnum}")
 img.figure.savefig(f"{datestr}_{genotype}_{RNAi}_{repnum}_annotated.png")      
 
 plt.close()
