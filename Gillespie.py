4cbd7c46c3cbdde2269dc870b79dcd38a264098b
David <davidcking.inbox@gmail.com>
2023-11-24 15:26:22 -0700

    Added new models

--- Gillespie.py
+++ Gillespie.py
@@ -13,6 +13,62 @@ def repression(x, beta):
 def activation(x, alpha):
   return x / (x + alpha)
 
+"""
+chatgpt, when asked to generate random variates of the time between negative binomial events with a given
+overdispersion:
+
+Generate the number of trials until r failures occur using a negative binomial distribution with parameters 
+r and p.
+
+Generate r overdispersion factors from a gamma distribution with shape parameter θ and scale parameter 
+1/λ.
+
+Generate r baseline waiting times between failures from an exponential distribution with rate parameter 
+λ.
+
+Multiply each baseline waiting time by the corresponding overdispersion factor.
+
+Sum the adjusted waiting times to obtain the time until the r-th failure in the negative binomial process.
+
+This procedure models the time between events in a negative binomial distribution while incorporating 
+overdispersion with the parameter θ. Adjust the distribution parameters and sample size based on your 
+specific requirements.
+"""
+
+import numpy as np
+
+def generate_time_between_events_with_overdispersion(r, p, theta, rate, size):
+  # Step 1: Generate number of trials until r failures
+  trials_until_r_failures = np.random.negative_binomial(r, p, size)
+
+  # Step 2: Generate overdispersion factors
+  overdispersion_factors = np.random.gamma(shape=theta, scale=1/rate, size=(size, r))
+
+  # Step 3: Generate baseline waiting times between failures
+  baseline_waiting_times = np.random.exponential(scale=1/rate, size=(size, r))
+
+  # Step 4: Multiply baseline waiting times by overdispersion factors
+  adjusted_waiting_times = baseline_waiting_times * overdispersion_factors
+
+  # Step 5: Sum adjusted waiting times to get time until r-th failure
+  time_until_r_failures = np.sum(adjusted_waiting_times, axis=1)
+
+  return trials_until_r_failures, time_until_r_failures
+
+def exaple_generate_random_time_between_nbinom_with_overdispersion_theta():
+  # Example usage
+  r = 3
+  p = 0.3
+  theta = 2.0  # Overdispersion parameter
+  rate = 0.2
+  size = 1000
+
+  trials, times_between_events = generate_time_between_events_with_overdispersion(r, p, theta, rate, size)
+
+# You can now analyze the 'times_between_events' array, which contains the time between events with overdispersion
+
+
+
 def Gillespie(N, V, y0, tlen, seed = None, spikes = []):
   '''N is the stoichiometry matrix
   rate function V. function that gives us the propensities as a function of the current state.
@@ -28,6 +84,7 @@ def Gillespie(N, V, y0, tlen, seed = None, spikes = []):
   i = 0
   status_msg = ""
   while True: #just continuously looping until there is a break from within
+
     """
     if i % 5e3 == 0:
       backspace = "\b" * len(status_msg)
@@ -42,6 +99,7 @@ def Gillespie(N, V, y0, tlen, seed = None, spikes = []):
 """
     i += 1
 
+    # prevent a runaway situation
     if sum(y) > 1e5:
       break
 
@@ -111,6 +169,73 @@ def buffering_two_bodies():
   plt.legend()
   plt.show()
 
+def two(X_init = 50, Y_init = 50, k_x = 1, g_x = 1, k_y = 1, g_y = 1, alpha = 50, beta = 50, tlen=50, seed=None):
+  stoich = np.array([[1,-1, 0, 0],
+                     [0, 0, 1, -1]])
+  
+  if alpha is not None:
+    f_alpha = lambda x: activation(x, alpha)
+  else: 
+    f_alpha = lambda x: 1*x
+
+  if beta is not None:
+    f_beta = lambda x: repression(x, beta)
+  else: 
+    f_beta = lambda x: 1*x
+
+  f_x = lambda x, y: x*k_x * f_alpha(y) * f_beta(x)
+  f_y = lambda x, y: y*k_y * f_alpha(x) * f_beta(y)
+
+
+  prop_functions = lambda y: [  f_x(y[0],y[1]), 
+                                g_x * y[0],
+                                f_y(y[0],y[1]),
+                                g_y * y[1]]
+
+  (ts, res) = Gillespie(stoich, prop_functions, [X_init, Y_init], tlen, seed = seed)
+  return (ts, res, seed)
+
+def main():
+  X_init = 200
+  Y_init = 200
+  k_x = 1.1
+  g_x = 1
+  k_y = 1.1
+  g_y = 1
+  alpha = .01
+  beta = 5000
+  seed = 1
+  tlen = 500
+  plot_xs = np.arange(0, tlen, tlen/1000)
+
+  gillespie_out = two(X_init = X_init, Y_init = Y_init, 
+      k_x= k_x, g_x = g_x, 
+      k_y= k_y, g_y = g_y, 
+      alpha = alpha, beta=beta,
+      tlen=tlen, seed=seed)  
+  x, y = interpolate_two(plot_xs, gillespie_out)
+  x.shape = plot_xs.shape
+  y.shape = plot_xs.shape
+
+  filename=f"Gillespie_sims/feedback/X{X_init}_kx{k_x}_Y{Y_init}_ky{k_y}_alpha{alpha}_beta{beta}_seed{seed}.png"
+
+  plt.figure(figsize=((20,5)))
+  plt.plot(plot_xs, x, label='X')
+  plt.plot(plot_xs, y, label='Y')
+  plt.ylim(bottom = 0)
+  plt.xlabel('Time')
+  plt.ylabel('Count')
+  titleStr = f"{X_init=}, {k_x=}, {Y_init=}, {k_y=},{alpha=}, {beta=}, {seed=}"
+  plt.title(titleStr)
+  plt.legend()
+  plt.savefig(filename)
+  plt.show()
+  
+  plt.close()
+
+  
+
+
 def simple(X_init = 50, k_x = 1, g_x = 1, beta = 50, tlen=50, seed=None):
   stoich = np.array([1,-1])
   # X_init = 50
@@ -146,8 +271,8 @@ def osc_V_yc(y,c):
 def oscillation():
 
   osc_N = np.array([[-1,0,1],
-                [1,-1,0],
-                [0,1,-1]])
+                    [1,-1,0],
+                    [0,1,-1]])
 
   y0 = np.array([200, 100, 100])
   c = 0.05
@@ -170,59 +295,45 @@ def oscillation():
   plt.legend()
   plt.show()
 
-def feedback():
-  #Gillespie2(X, Y, tlen, alpha, beta, params, seed = None)
-  tlen = 1000
-  params = {
-    'k_x': 1.125,
-    'g_x': 1,
-    'k_y': 1,
-    'g_y': 1,
-    'alpha': 0.25,
-    'beta': 1e3
-  }
-  X0 = [10]
-  Y0 =[10]
-  (ts, X, Y) = Gillespie2(X0,Y0,500,params)
-  plt.figure()
-  plt.plot(ts, X, label='X')
-  plt.plot(ts, Y, label='Y')
-  #plt.plot(ts, zcounts, label='Z')
-  plt.xlabel('Time')
-  plt.ylabel('Count')
-  titleStr = '#X0='+str(X[0])+', #Y0='+str(Y[0]) #+', #Z0='+str(y0[2])+', c='+str(c)
-  plt.title(titleStr)
-  plt.legend()
-  plt.show()
-
 def interpolate(plot_xs, gillespie_out):
   ts, res, _ = gillespie_out
   xcounts = res[:,0]
   interp = np.interp(plot_xs, ts, xcounts)
   interp.shape = (1, len(interp))
   return interp
-import os 
+
+def interpolate_two(plot_xs, gillespie_out):
+  ts, res, _ = gillespie_out
+  xcounts = res[:,0]
+  ycounts = res[:,1]
+  interp_x = np.interp(plot_xs, ts, xcounts)
+  interp_x.shape = (1, len(interp_x))
+  interp_y = np.interp(plot_xs, ts, ycounts)
+  interp_y.shape = (1, len(interp_y))
+  return interp_x, interp_y
+
+import os  # for getpid()
 def launchfunc(argtuple):
   plot_xs, X_init, k_x, g_x, beta, tlen, seed, i = argtuple
   print(f"Running simple() on {os.getpid()}. {i=}, {seed=}", file=sys.stderr)
   return interpolate( plot_xs, simple(X_init = X_init, k_x= k_x, g_x = g_x, beta = beta, tlen=tlen, seed=seed))
 
-def main():
-  plt.figure()
+def main_simple():
+  fig, ax = plt.subplots(2)
 
-  plt.xlabel('Time')
-  plt.ylabel('Count')
+  ax[0].set_xlabel('Time')
+  ax[0].set_ylabel('Count')
 
-  N = 20 # number of simulations to plot
+  N = 1000 # number of simulations to aggregate
   random.seed(1) # change this to vary the simulations
   seeds = random.sample(range(10000), k = N)
-  tlen = 15 # total time (x limit of plot)
+  tlen = 50 # total time (x limit of plot)
 
   # model parameters
   X_init = 1000
-  k_x = 1
+  k_x = 1.5
   g_x = 1
-  beta = None #1000
+  beta = 1000
   
   # smoothe
   plot_xs = np.arange(0, tlen, tlen/1000)
@@ -234,7 +345,7 @@ def main():
   MP = True
   if MP:
     import multiprocessing as mp
-    with mp.Pool(processes = 5) as p:
+    with mp.Pool(processes = 6) as p:
       arglist = [(plot_xs, X_init, k_x, g_x, beta, tlen, seed, i) for (i,seed) in enumerate(seeds)]
       interps = p.map(launchfunc,arglist)
   else:
@@ -249,9 +360,9 @@ def main():
 
   mean = np.mean(concat, axis=0)
   sd = np.std(concat, axis=0)
-  for q in np.arange(0,1,.1):
+  for q in np.arange(0,1.125,.125):
     qvec = np.quantile(concat, q, axis=0)
-    plt.plot(plot_xs, qvec, label="%.1f" % q)
+    ax[0].plot(plot_xs, qvec, label="%.2f" % q)
 
   q0 = np.quantile(concat, 0, axis=0)
   q1 = np.quantile(concat, .25, axis=0)
@@ -260,14 +371,24 @@ def main():
   q4 = np.quantile(concat, 1, axis=0)
 
   print(f"{X_init=}, {k_x=}, {g_x=}, {beta=}, iqr={q3[-1]-q1[-1]}")
-  iqr = round(q3[-1]-q1[-1],2)
-  titleStr = f"{X_init=}, {k_x=}, {g_x=}, {beta=}, {iqr=}"
-  #titleStr = f"{X_init=}, {k_x=}, {g_x=}"
-  plt.title(titleStr)
-  plt.legend()
-  plt.ylim(bottom = 0)
-  plt.show()
+  iqr_end = round(q3[-1]-q1[-1],2)
   
+  titleStr = f"{X_init=}, {k_x=}, {g_x=}, {beta=}, {iqr_end=}"
+  #titleStr = f"{X_init=}, {k_x=}, {g_x=}"
+  ax[0].set(title=titleStr)
+  ax[0].legend()
+  bottom, top = ax[0].get_ylim()
+  ax[0].set_ylim(0, top, auto=None)
+
+
+  
+  iqr_total = q3 - q1
+  #plt.figure()
+  ax[1].set(title="Increase in IQR over time")
+  ax[1].set_xlabel('Time')
+  ax[1].set_ylabel('IQR')
+  ax[1].plot(plot_xs, iqr_total)
+  plt.show()
   #buffering_two_bodies()
 
 if __name__ == '__main__': main()
